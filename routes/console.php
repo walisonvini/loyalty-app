94dd11f8adda65c62a5ecbcc8b22b2cc8c4db0ab
walisonvini <walison.vinicios12@gmail.com>
2025-09-05 17:45:34 -0300

    Atualiza o fuso horário para 'America/Sao_Paulo' e adiciona comando agendado para notificar clientes elegíveis ao prêmio máximo diariamente às 09:00.

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,6 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+Schedule::command('loyalty:notify-max-reward-eligible')
+    ->dailyAt('09:00');
