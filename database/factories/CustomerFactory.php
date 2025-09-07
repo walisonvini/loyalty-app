462ba449a25c66777df5bac1d74e6a16da80365e
walisonvini <walison.vinicios12@gmail.com>
2025-09-06 22:40:46 -0300

    Adicionar CustomerFactory e CustomerSeeder para gerar dados de clientes

---
+++ database/factories/CustomerFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Customer>
+ */
+class CustomerFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'cpf' => fake()->unique()->numerify('###########'),
+            'phone' => fake()->unique()->numerify('##9########'),
+            'points_balance' => fake()->numberBetween(0, 100),
+        ];
+    }
+}
